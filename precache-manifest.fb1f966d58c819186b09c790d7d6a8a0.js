940a02176a3d09e099d7ca154c6beeaedd90d22f
William Pei Yuan <williampeiyuan@gmail.com>
2018-10-16 17:35:32 -0400

    Updates

---
+++ precache-manifest.fb1f966d58c819186b09c790d7d6a8a0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dbb564be5126d01a2a81fdd81a9013b",
+    "url": "/typecode-challenge/static/media/logo_and_hamburger.0dbb564b.svg"
+  },
+  {
+    "revision": "31728951f2b96695e232",
+    "url": "/typecode-challenge/static/css/main.5158bcdd.chunk.css"
+  },
+  {
+    "revision": "7944ef19780679bef348",
+    "url": "/typecode-challenge/static/js/1.7944ef19.chunk.js"
+  },
+  {
+    "revision": "3e1fdc4aa71bd528c172",
+    "url": "/typecode-challenge/static/js/runtime~main.3e1fdc4a.js"
+  },
+  {
+    "revision": "499e65741cb085eb431a41d869744ea2",
+    "url": "/typecode-challenge/static/media/checkmark.499e6574.svg"
+  },
+  {
+    "revision": "80a148ac61e9ba55e282ba5dfaf9bb73",
+    "url": "/typecode-challenge/static/media/pencil.80a148ac.svg"
+  },
+  {
+    "revision": "bb559d615ab572e9aef602afc24f7d15",
+    "url": "/typecode-challenge/static/media/x.bb559d61.svg"
+  },
+  {
+    "revision": "31728951f2b96695e232",
+    "url": "/typecode-challenge/static/js/main.31728951.chunk.js"
+  },
+  {
+    "revision": "f550205a3636e296cb7f0b676a866352",
+    "url": "/typecode-challenge/static/media/Copernicus-Heavy.f550205a.ttf"
+  },
+  {
+    "revision": "f0545a7b9978ac33c809988d4d7a42b8",
+    "url": "/typecode-challenge/static/media/Copernicus-Heavy.f0545a7b.woff"
+  },
+  {
+    "revision": "dc60fd3984b2824468fefb0379c59ea2",
+    "url": "/typecode-challenge/static/media/Copernicus-Heavy.dc60fd39.eot"
+  },
+  {
+    "revision": "ab2b4caec42e13ac31a218c611bf6c3e",
+    "url": "/typecode-challenge/static/media/Copernicus-Heavy.ab2b4cae.svg"
+  },
+  {
+    "revision": "7a21882b3df29a4d08ce88cf2545231d",
+    "url": "/typecode-challenge/static/media/wildernessBcg.7a21882b.jpg"
+  },
+  {
+    "revision": "8435c200686d846866866a416e71bc14",
+    "url": "/typecode-challenge/index.html"
+  }
+];
